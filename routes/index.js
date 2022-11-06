e2477a754d502c1daef89d2ee2d25e4df0ae48be
zachjhavers <zachjhavers@gmail.com>
2022-11-06 15:36:15 -0500

    Fith Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET surveys page. */
+router.get('/surveys', function(req, res, next) {
+  res.render('surveys', { title: 'Surveys' });
 });
 
 module.exports = router;
