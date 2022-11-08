89f548a79eab056a28707f339754240e08a68cae
zachjhavers <zachjhavers@gmail.com>
2022-11-07 21:06:17 -0500

    Sixth Commit

--- AlphardSurvey/src/app/app.module.ts
+++ AlphardSurvey/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { LoginBoxComponent } from './components/login-box/login-box.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    LoginBoxComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ AlphardSurvey/src/app/components/login-box/login-box.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginBoxComponent } from './login-box.component';
+
+describe('LoginBoxComponent', () => {
+  let component: LoginBoxComponent;
+  let fixture: ComponentFixture<LoginBoxComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginBoxComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginBoxComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
