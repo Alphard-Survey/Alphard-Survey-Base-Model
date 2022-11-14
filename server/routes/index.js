fd8f8f07283d856c4a970adbd57841fe26332fb9
David02P <pietrocoladavid@gmail.com>
2022-11-14 10:46:39 -0500

    Adding comments to each file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,13 @@
+/*
+Members (Name - Student ID)
+David Pietrocola –301247544 
+Jacob Todasco – 301251200 
+Jungyu Lee – 301236221 
+Menal Humeda – 301160220 
+Ryan Arafeh – 301239052 
+Zack Havers – 301202845 
+*/
+
 var express = require('express');
 var router = express.Router();
 
