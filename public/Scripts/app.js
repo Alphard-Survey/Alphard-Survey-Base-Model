fe61ae323dbac8b30f7ce61d7ba20f147eed4cb6
menalhumeda <mhumeda@my.centennialcollege.ca>
2022-12-03 20:19:53 -0500

    SEND BUTTON

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,4 @@
+
 // IIFE -- Immediately Invoked Function Expression
 (function () {
   function Start() {
@@ -13,4 +14,50 @@
     }
   }
   window.addEventListener("load", Start);
+})();
+
+(function () {
+ 
+  function sendinfo()
+  {
+    var name = document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+    var subject= document.getElementById("subject").value;
+    var comment = document.getElementById("comment").value;
+
+    if(name =="")
+    {
+      alter("Please enter your Name!")
+    }
+
+    if(email=="")
+   { alert("Please enter your Email!")}    
+
+   if(subject=="")
+   {
+    alert("Please enter the Subject!")
+   }
+   if(comment=="")
+   {
+    alert("Please enter your Comment!")
+   }
+    else if(name !="" && email !="" && subject !="" && comment != "")
+    
+  
+    {
+      alert(" DEAR CUSTOMER THANK YOU FOR CONTACING US !");
+    }
+
+    else
+    {
+      alert();
+    }
+    
+   
+  }
+
+  var send = document.getElementById("send");
+     send.addEventListener("click", sendinfo, false);
+
+
 })();
