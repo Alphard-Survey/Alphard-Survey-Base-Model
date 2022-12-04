46a59545194d86cd52192ad8ee4a0169016938c1
menalhumeda <mhumeda@my.centennialcollege.ca>
2022-12-03 20:43:22 -0500

    send button

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -14,3 +14,51 @@
   }
   window.addEventListener("load", Start);
 })();
+
+
+(function () {
+ 
+  function sendinfo()
+  {
+    var name = document.getElementById("name").value;
+    var email= document.getElementById("email").value;
+    var subject= document.getElementById("subject").value;
+    var comment = document.getElementById("comment").value;
+
+    if(name =="")
+    {
+      alter("Please enter your Name!")
+    }
+
+    if(email=="")
+   { alert("Please enter your Email!")}    
+
+   if(subject=="")
+   {
+    alert("Please enter the Subject!")
+   }
+   if(comment=="")
+   {
+    alert("Please enter your Comment!")
+   }
+    else if(name !="" && email !="" && subject !="" && comment != "")
+    
+  
+    {
+      alert(" DEAR CUSTOMER THANK YOU FOR CONTACING US !");
+    }
+
+    else
+    {
+      alert();
+    }
+    
+   
+  }
+
+  var send = document.getElementById("send");
+     send.addEventListener("click", sendinfo, false);
+
+
+})();
+
